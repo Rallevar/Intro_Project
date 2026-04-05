b669b60450d73adbd9f838010f25d8a220b3d443
Rallevar <nkeenan6969@gmail.com>
2026-04-04 19:54:33 -0500

    Added content to Wards and Vendors views. Toggleable sorting helper added for all pages.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,4 +1,16 @@
 class VendorsController < ApplicationController
   def index
+  # Get the column to sort by from the URL (default to 'id' if none)
+  sort_by = params[:sort] || 'id'
+
+  # Get the direction from the URL (default to 'asc' if none)
+  if params[:direction] == 'desc'
+    sort_dir = 'desc'
+  else
+    sort_dir = 'asc'
+  end
+
+  # Sort the vendors using the column and direction
+  @vendors = Vendor.order(sort_by + ' ' + sort_dir)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def toggle_direction(column)
+    if params[:sort] == column && params[:direction] == 'asc'
+      'desc'
+    else
+      'asc'
+    end
+  end
 end
